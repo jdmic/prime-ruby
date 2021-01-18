0daa207a324b271a23e9597b4280e36b6646b5a9
James <james.michalec58@gmail.com>
2021-01-17 17:03:10 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+require "pry"
+
+def prime?(interger)
+    # make a list of numbers from 2 to interger
+    range1=(2..interger-1).to_a  
+    count = 0
+    if interger <= 1
+       return false
+    else
+        range1.each do |number|
+            if interger % number >= 1
+                count += 1
+            else 
+                return false
+            end
+        end
+    end
+    if count == 0
+        return true
+    else
+        return false
+end
